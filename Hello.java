36d3a65c1fe9a865617ad0d70e00c6c6372cb95f
Bradley Thomas <bradley.j.thomas31@gmail.com>
2022-08-30 11:20:56 -0400

    adding scanner

--- Hello.java
+++ Hello.java
@@ -2,6 +2,9 @@ public class Hello {
    public static void main(String[] args) {
       System.out.println("Hello world.");
       System.out.println("saying hello to mx w");
+       java.util.Scanner keyboard = new java.util.Scanner(System.in);
+       System.out.println("How are you?");
+       String response = keyboard.nextLine();
    }
 }
 
