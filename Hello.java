73a6d913537b62cd0e39c485063d2f69dff4d675
Bradley Thomas <bradley.j.thomas31@gmail.com>
2022-08-30 11:56:12 -0400

    fixed import

--- Hello.java
+++ Hello.java
@@ -1,8 +1,9 @@
+import java.util.Scanner;
 public class Hello {
    public static void main(String[] args) {
       System.out.println("Hello world.");
       System.out.println("saying hello to mx w");
-       java.util.Scanner keyboard = new java.util.Scanner(System.in);
+      Scanner keyboard = new java.util.Scanner(System.in);
        System.out.println("How are you?");
        String response = keyboard.nextLine();
        System.out.println("more talk");
